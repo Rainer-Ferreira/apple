94f0ef193e37bcfdcf75180ae849e9093fa8f46d
Rainer-Ferreira <rainerferreira.fisio@gmail.com>
2025-05-20 23:39:56 -0300

    Criando if e else da aplicação

--- scripts.js
+++ scripts.js
@@ -7,3 +7,31 @@ const list = document.querySelector('.list')
 
 let active = 0;
 const total = items.length
+let timer;
+
+
+function update(direction) {
+
+    document.querySelector('.item.active').classList.remove('active')
+    document.querySelector('.dot.active').classList.remove('active')
+
+
+    if (direction > 0) {
+        active = active + 1
+    }
+
+    else if (direction < 0) {
+
+    }
+
+}
+
+
+
+prevButton.addEventListener('click', function () {
+    update(-1)
+})
+
+nextButton.addEventListener('click', function () {
+    update(1)
+})
